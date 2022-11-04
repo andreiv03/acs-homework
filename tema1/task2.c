3a9a7b0bdbef4fbeedebd5d1ef6da45234183aeb
Andrei Voicu <andrei.voicu110@gmail.com>
2022-11-04 23:23:15 +0200

    New

---
+++ tema1/task2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include "utils.h"
+
+int main() {
+  int N = 0;
+  scanf("%d", &N);
+
+  float operands[N];
+  for (int index = 0; index < N; ++index)
+    scanf("%f", &operands[index]);
+
+  char operations[N];
+  scanf("%s", operations);
+
+  for (int index = 0; index < N - 1; ++index)
+    if (operations[index] == '*' || operations[index] == '/')
+      operands[index + 1] = applyOperation(operations[index], operands[index], operands[index + 1]);
+
+  for (int index = 0, counter = 1; index < N; ++index) {
+    if (operations[index] == '+' || operations[index] == '-') {
+      while (operations[index + counter] == '*' || operations[index + counter] == '/')
+        counter = counter + 1;
+
+      operands[index + counter] = applyOperation(operations[index], operands[index], operands[index + counter]);
+      counter = 1;
+    }
+  }
+
+  printf("%f\n", operands[N - 1]);
+  return 0;
+}
