004c1dfe3a0c117be8313cf2bfd362ae32350eb0
Andrei Voicu <andrei.voicu110@gmail.com>
2022-11-24 22:58:05 +0200

    New

---
+++ tema2/task4.c
@@ -0,0 +1,4 @@
+#include "./utils.h"
+
+void SolveTask4(void *info, int nr_avioane, int nr_obstacole, int *x, int *y, int N) {
+}
---
+++ tema2/task5.c
@@ -0,0 +1,4 @@
+#include "./utils.h"
+
+void SolveTask5(void *info, int nr_avioane, int T, int nr_pct_coord, int *X, int *Y, int N) {
+}
