ca41b98459f3758fb93a00adbcee43fbb27becf9
Andrei Voicu <andrei.voicu110@gmail.com>
2023-05-04 00:57:57 +0300

    Create task-3.m

---
+++ tema1/mnist-101/task-3.m
@@ -0,0 +1,10 @@
+% Copyright (c) 2023 Numerical Methods Team
+% DO NOT MODIFY this file
+
+source("./load_dataset.m")
+source("./split_dataset.m")
+source("./initialize_weights.m")
+source("./cost_function.m")
+source("./predict_classes.m")
+source("./sigmoid.m")
+source("./fmincg.m")
