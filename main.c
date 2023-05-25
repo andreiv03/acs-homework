ee79803259536df2cd127d4e0360389ddd30c846
bubu2281 <115945380+bubu2281@users.noreply.github.com>
2023-05-25 18:29:09 +0300

    Update main.c
    Deocamdata e implementat doar meniul

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include "screen_functions.h"
 
 int main() {
     initscr();
-    printf("mama");
+    noecho();
+    TMenu menu;
+    menu.difficulty = 1;
+    char choice = '?';
+    while ((choice != '1') && (choice != '3')) {
+        PrintMenu(&menu, &choice);
+    }
+    
     endwin();
     return 0;
 }
